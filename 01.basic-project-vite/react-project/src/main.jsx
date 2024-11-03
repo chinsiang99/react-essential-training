47a251d6bfe9e990b51b1689e88f96aa55d14062
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-03 19:27:24 +0800

    feat: working with destructuring array

--- 01.basic-project-vite/react-project/src/main.jsx
+++ 01.basic-project-vite/react-project/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const animals = ["giraffe", "zebra", "tiger"]
+const [first, second, third] = ["giraffe", "zebra", "tiger"]
+console.log(animals[0])
+console.log(first)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
