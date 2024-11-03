d5fc70e3c0f933a91ccf30bd34cf610bd0072491
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-03 19:51:25 +0800

    feat: working with use state hook

--- 01.basic-project-vite/react-project/src/App.jsx
+++ 01.basic-project-vite/react-project/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import chef from './images/chef.jpg'
+import { useState } from 'react'
 
 function Header(props){
   const { name, year } = props
@@ -43,8 +44,12 @@ function Main({dishes}){
 let language = "Javascript"
 let moon = "🌙"
 function App() {
+  const [status, setStatus] = useState("open")
+  console.log(status)
   return (
     <div>
+        <h1>The restaurant is currently {status}</h1>
+        <button onClick={() => setStatus(status === "open" ? "close": "open")}>Close/Open restaurant</button>
         <Header name="Alex" year={new Date().getFullYear()}/>
         <h1>Hello {language.toUpperCase()} {moon}!</h1>
         <Main dishes = {dishObjects}/>
