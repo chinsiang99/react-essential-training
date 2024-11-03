529676f6d5ed1f971400c3d2674409ebcd30f66e
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-03 12:58:00 +0800

    feat: working with creating a new react component

--- 01.basic-project-vite/react-project/src/App.jsx
+++ 01.basic-project-vite/react-project/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+function Header(){
+  return(
+    <header>
+      <h1>This is a sample header</h1>
+    </header>
+  )
+}
+
 let language = "Javascript"
 let moon = "🌙"
 function App() {
   return (
-    <h1>Hello {language.toUpperCase()} {moon}!</h1>
+    <>
+        <h1>Hello {language.toUpperCase()} {moon}!</h1>
+        <Header></Header>
+    </>
   )
 }
 
